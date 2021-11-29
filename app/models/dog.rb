83d90d7fd2d1d3d828a2c23a5b74d57a82967e45
Neil Pulcyn <neilpulcyn@hotmail.co.uk>
2021-11-29 17:00:53 +0000

    mapbox implemented and javascript code amended for ajax searchbar

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -3,6 +3,8 @@ class Dog < ApplicationRecord
   belongs_to :user
   has_many :bookings
   validates :name, :size, :age, :description, :gender, :address, :breed, presence: true
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 
   include PgSearch::Model
   pg_search_scope :search_by_breed_and_size_and_gender,
